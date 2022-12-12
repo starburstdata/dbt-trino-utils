156fa7d7f7d961f6e8236dab110a3ffb8822ad3e
Damian Owsianny <damian.owsianny@starburstdata.com>
2022-12-12 16:30:44 +0100

    Remove cross-db macros, as they have moved to the dbt namespace

--- macros/dbt_utils/cross_db_utils/array_append.sql
+++
@@ -1,3 +0,0 @@
-{% macro trino__array_append(array, new_element) -%}
-    {{ dbt_utils.array_concat(array, dbt_utils.array_construct([new_element])) }}
-{%- endmacro %}
--- macros/dbt_utils/cross_db_utils/array_construct.sql
+++
@@ -1,7 +0,0 @@
-{% macro trino__array_construct(inputs, data_type) -%}
-    {%- if not inputs -%}
-    NULL
-    {%- else -%}
-    ARRAY[ {{ inputs|join(' , ') }} ]
-    {%- endif -%}
-{%- endmacro %}
