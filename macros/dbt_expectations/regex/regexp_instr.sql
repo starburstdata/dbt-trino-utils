2e505ea0cc607b899d21f4f2408be08ef330d727
aezomz <aezoteo@gmail.com>
2023-03-14 16:29:30 +0800

    add flags new optional field to regexp_instr

--- macros/dbt_expectations/regex/regexp_instr.sql
+++ macros/dbt_expectations/regex/regexp_instr.sql
@@ -1,4 +1,5 @@
 
-{% macro trino__regexp_instr(source_value, regexp, position, occurrence, is_raw) %}
+{% macro trino__regexp_instr(source_value, regexp, position, occurrence, is_raw, flags) %}
 regexp_position({{ source_value }}, '{{ regexp }}', {{ position }}, {{ occurrence }})
 {% endmacro %}
+
