8ede8ebe2feb1509522bde32737c1ca5ebca7ff6
Damian Owsianny <damian3031@gmail.com>
2023-03-30 16:00:02 +0200

    Add integration tests for dbt-date

---
+++ docker/run_dbt_date_integration_tests.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# move to wherever we are so docker things work
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+set -exo pipefail
+docker run \
+    --network="dbt-net" \
+    -v $PWD/dbt:/root/.dbt \
+    dbt-trino-utils \
+    "cd /opt/dbt_trino_utils/integration_tests/dbt_date \
+        && dbt deps \
+        && dbt seed \
+        && dbt run \
+        && dbt test"
