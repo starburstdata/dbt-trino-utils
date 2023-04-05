57ffa046f6db0ad91c6d72e36328bfad66f76c33
Damian Owsianny <damian3031@gmail.com>
2023-04-05 13:10:27 +0200

    Add trino__fact_orders_source seed, adjust metrics and models to it. It is needed to override fact_orders_source seed, as Trino can't cast string formatted 'mm/dd/yyyy' to date.

---
+++ integration_tests/dbt_metrics/models/metric_testing_models/trino__develop_metric.sql
@@ -0,0 +1,45 @@
+{% set my_metric_yml -%}
+{% raw %}
+
+metrics:
+  - name: develop_metric
+    model: ref('trino__fact_orders')
+    label: Total Discount ($)
+    timestamp: order_date
+    time_grains: [day, week, month]
+    calculation_method: average
+    expression: discount_total
+    dimensions:
+      - had_discount
+      - order_country
+
+  - name: derived_metric
+    label: Total Discount ($)
+    timestamp: order_date
+    time_grains: [day, week, month]
+    calculation_method: derived
+    expression: "{{ metric('develop_metric') }} - 1 "
+    dimensions:
+      - had_discount
+      - order_country
+
+  - name: some_other_metric_not_using
+    label: Total Discount ($)
+    timestamp: order_date
+    time_grains: [day, week, month]
+    calculation_method: derived
+    expression: "{{ metric('derived_metric') }} - 1 "
+    dimensions:
+      - had_discount
+      - order_country
+
+{% endraw %}
+{%- endset %}
+
+select * 
+from {{ metrics.develop(
+        develop_yml=my_metric_yml,
+        metric_list=['derived_metric'],
+        grain='month'
+        )
+    }}
