4179a6734480be92d24aa0bd7540b387ac5bfdc4
Willy Hardy <whardy@redhat.com>
2023-10-05 16:22:43 -0400

    only override expectations

--- macros/dbt_expectations/regex/regexp_instr.sql
+++ macros/dbt_expectations/regex/regexp_instr.sql
@@ -1,4 +1,15 @@
-
-{% macro trino__regexp_instr(source_value, regexp, position, occurrence, is_raw) %}
+{% macro trino__regexp_instr(source_value, regexp, position=1, occurrence=1, is_raw=False, flags="") %}
+{% if flags %}
+    {{ exceptions.warn(
+            "flags option is not supported for this adapter "
+            ~ "and is being ignored."
+    ) }}
+{% endif %}
+{% if is_raw %}
+    {{ exceptions.warn(
+            "is_raw option is not supported for this adapter "
+            ~ "and is being ignored."
+    ) }}
+{% endif %}
 regexp_position({{ source_value }}, '{{ regexp }}', {{ position }}, {{ occurrence }})
 {% endmacro %}
--- macros/dbt_utils/schema_cleanup/drop_schema_by_name.sql
+++
@@ -1,4 +0,0 @@
-{% macro trino__drop_schema_by_name(schema_name) %}
-    {% set relation = api.Relation.create(database=target.database, schema=schema_name) %}
-    {% do drop_schema(relation) %}
-{% endmacro %}
